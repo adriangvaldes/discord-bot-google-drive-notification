66e2caed3fb9ea9b61ef09ea86adc0a478cb4abb
Adrian Valde <adriangvaldes@gmail.com>
2024-06-08 17:54:57 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+)
+
+const (
+	discordToken      = "MTI0OTA3NDY3ODA5MDMwMTU0Mg.GFdxy_.hbPQ9tOmW2TD4W1VfrEpq6G2FUZYBFLf1E1Mog"
+	discordChannelID  = "1230687636340080640"
+	googleCredentials = "./google-drive-credentials.json"
+	fileID            = "1-C8KRfua1gqy-wj81e9oTyeWmzYO6oS3"
+)
+
+func main() {
+	// Start Discord bot
+	dg, err := discordgo.New("Bot " + discordToken)
+	if err != nil {
+		log.Fatalf("Error creating Discord session: %v", err)
+	}
+
+	err = dg.Open()
+	if err != nil {
+		log.Fatalf("Error opening Discord session: %v", err)
+	}
+	defer dg.Close()
+
+	// Setup Google Drive API client
+	b, err := os.ReadFile(googleCredentials)
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	config, err := google.ConfigFromJSON(b, drive.DriveMetadataReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	client := getClient(config)
+	srv, err := drive.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Drive client: %v", err)
+	}
+
+	// Monitor file updates
+	lastModifiedTime := time.Now()
+	for {
+
+		// Get file metadata with specific fields
+		file, err := srv.Files.Get(fileID).Fields("modifiedTime").Do()
+		if err != nil {
+			log.Printf("Unable to retrieve file metadata: %v", err)
+			continue
+		}
+
+		modifiedTime, err := time.Parse(time.RFC3339, file.ModifiedTime)
+		if err != nil {
+			log.Printf("Unable to parse modified time: %v", err)
+			continue
+		}
+
+		if modifiedTime.After(lastModifiedTime) {
+			lastModifiedTime = modifiedTime
+
+			fmt.Printf("Novo APK disponivel!\n[Link Acesso](https://drive.google.com/file/d/1-C8KRfua1gqy-wj81e9oTyeWmzYO6oS3/view?usp=sharing)")
+
+			// message := fmt.Sprintf("O documento foi atualizado em: %s", modifiedTime)
+			// _, err := dg.ChannelMessageSend(discordChannelID, message)
+			// if err != nil {
+			// 	log.Printf("Unable to send message to Discord: %v", err)
+			// }
+		}
+		time.Sleep(5 * time.Second)
+	}
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	tokenFile := "token.json"
+	tok, err := tokenFromFile(tokenFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokenFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.Background(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.Create(path)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
